300f9b8572592f86195e6643b9a7677fe5b25f90
Jordix69 <laracisnerosjairo@gmail.com>
2025-03-20 15:27:04 -0600

    paquete

--- integradora/src/views/PaqueteSpace.vue
+++ integradora/src/views/PaqueteSpace.vue
@@ -1,20 +1,26 @@
 <template>
-    <NavBar />
+    <NavBar @scroll-to-contacto="handleScrollToContacto" />
     <div class="Bienvenida" ><h1 style="color: aliceblue;" >PAQUETE</h1></div>
     <div class="paq" style="background-color: #F1C5FC;" >
         <img style="width: 40%;height: 40%; " src="../imagenes/oculus-removebg-preview 1.png" alt="">
         <span style="font-size: 120px; margin-right: 20px;" >+</span>
-        <p style="font-size: 60px;text-align: center; " >El modelo de tu negocio en realidad aumentada</p>
+        <p style="font-size: 60px;text-align: center; " >Tu negocio en una nueva realidad</p>
     </div>
     <ContactoC />
   </template>
   
   <script setup lang="ts">
-  import { ref, onMounted } from "vue";
-  import { useRoute, useRouter } from "vue-router";
-  import NavBar from "@/components/NavBar.vue";
-  import ContactoC from "@/components/ContactoC.vue";
-  </script>
+import { useRouter } from "vue-router";
+import { onMounted } from "vue";
+import NavBar from "@/components/NavBar.vue";
+import ContactoC from "@/components/ContactoC.vue";
+
+const router = useRouter();
+
+const handleScrollToContacto = () => {
+  router.push({ path: "/", query: { scrollTo: "contacto" } });
+};
+</script>
   
 <style scoped>
 .paq {
